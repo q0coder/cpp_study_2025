53bbdd7f7833f05677155ec2670a67f9e06dc394
Qcoder <3185566911@qq.com>
2025-12-07 21:13:41 +0800

    Add files via upload

---
+++ cpp_study_25_12_7.cpp
@@ -0,0 +1,99 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<string>
+#include<fstream>
+#include<complex>
+using namespace std;
+//int main()
+//{
+//	string name ( "уехЩ");
+//	complex<double> num_i(1, 2);
+//	cout << name
+//		<< " "
+//		<< num_i;
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	ofstream outfile("0.txt");
+//	outfile << "123";
+//	return 0;
+//}
+//
+
+//int main()
+//{
+//	ofstream ofs;
+//	ofs.open("1.txt", ios::out);
+//	ofs << "3";
+//	ofs.close();
+//	return 0;
+//}
+
+//int main()
+//{
+//	string name;
+//	ifstream infile("0.txt");
+//	infile >>name;
+//	cout << name;
+//	return 0;
+//}
+
+//int main()
+//{
+//	fstream iofile("0.txt", ios_base::in | ios_base::app);
+//	
+//	string num;
+//	iofile >> num;
+//	cout << num;
+//	
+//	system("pause");
+//	return 0;
+//}
+//#include<vector>
+//int main()
+//{
+//
+//	vector<int>vec = {1,2,3};
+//	
+//	cout << vec[0];
+//	system("pause");
+//	return 0;
+//}
+//#include<cstdlib>
+//int main()
+//{
+//	int a = 0;
+//	if (a == 0)
+//		exit(-1);
+//	cout << a;
+//	return 0;
+//}
+//int c(int a)
+//{
+//	if (a > 0)
+//		;
+//	else
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//	cin >> a;
+//	int ret=c(a);
+//	cout << ret;
+//
+
+
+//}
+
+
+int main()
+{
+	new int[20];
+
+	return 0;
+}
