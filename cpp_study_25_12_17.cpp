3f31a324ba6af50bec7a76255758a0efc14ee938
Qcoder <3185566911@qq.com>
2025-12-17 20:15:10 +0800

    Add files via upload

---
+++ cpp_study_25_12_17.cpp
@@ -0,0 +1,112 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+//int main()
+//{
+//	/*const int size = 10;
+//	int arr[size];*/
+//	/*string str1;
+//	cin >> str1;
+//	cout << str1 << endl;
+//string str2;
+//cin >> str2;
+//cout <<str2<< endl;*/
+////cin.getline()
+//	//cin.get();
+//
+//	return 0;
+//}
+
+
+//constexpr int Std::m_age;
+//
+//class Std
+//{
+//public:
+//	static constexpr int m_age=18;
+//	static  string m_name;
+//	 int score;
+//	//Std score;
+//	/*void out(int sc=score)
+//	{
+//		cout<<m_name;
+//	}*/
+//	static void print() {}
+//private:
+//	void setout()
+//	{
+//
+//	}
+//
+//};
+//
+//int main()
+//{
+//	Std::m_name;
+//	Std s;
+//	s.m_name;
+//	return 0;
+//}
+
+
+//
+//int main()
+//{
+//	/*cout << "!" << endl << "?" <<"\n";
+//	cout << "!" << ends << "?" << "\n";
+//	cout << "!" << flush << "?" << "\n";*/
+//	//string str1;
+//	cout << unitbuf;
+//	cout << nounitbuf;
+//	char str1[20];
+//	cin.get(str1, 20);
+//	cout << str1<<flush;
+//	char str2[20];
+//	cin.get();
+//	cin.get(str2, 20);
+//	cout << str2;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	//cin.tie(&cout);
+//	cin.tie(nullptr);
+//	auto p = cin.tie();
+//	cout << p << endl;
+//
+//
+//	return 0;
+//}
+
+#include<fstream>
+#include<string>
+//int main()
+//{
+//	ofstream ofs;
+//	ofs.open("test.txt", ios::out);
+//	ofs << "abcd";
+//	ofs.close();
+//	ifstream ifs;
+//	ifs.open("test.txt", ios::in);
+//	string c;
+//	while (getline(ifs, c))
+//		cout << c << endl;
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	ofstream outfile("test.txt");
+//	outfile << "abcd";
+//	outfile.close();
+//	string c;
+//	ifstream infile("test.txt");
+//	cout << infile.is_open() << endl;;
+//	infile >> c;
+//	cout << c<<endl;
+//
+//	return 0;
+//}
