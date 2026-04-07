b836631b95dbf05c973d1949fa590a5babcdbb61
Qcoder <3185566911@qq.com>
2026-04-07 20:52:58 +0800

    Add files via upload

---
+++ cpp_study_虚拟地址空间划分.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+
+int gdata1 = 10;
+int gdata2 = 0;
+int gdata3;
+
+static int gdata4 = 10;
+static int gdata5 = 0;
+static int gdata6;
+
+int main()
+{
+	int a = 10;
+	int b = 0;
+	int c;
+
+	static int e = 10;
+	static int f = 0;
+	static int g;
+
+}
