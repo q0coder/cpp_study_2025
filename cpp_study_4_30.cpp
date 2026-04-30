80daddfa88b1a13dc0f3cd0d30489547aa3dfda4
Qcoder <3185566911@qq.com>
2026-04-30 21:49:56 +0800

    Add files via upload

---
+++ cpp_study_4_30.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+//string func(string s1, string s2)
+//{
+//	string temp = s1 + s2;
+//	return temp;
+//}
+//
+//int main()
+//{
+//	string s = func(s1, s2);
+//}
+
+//struct a
+//{
+//	
+//
+//	char c;
+//	char c1;
+//	char c2;
+//
+//};
+//
+//int main()
+//{
+//	cout<<sizeof(a);
+//}
+
